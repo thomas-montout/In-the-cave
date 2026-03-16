2d5e9befa4681a66ed0f2e8221ff0ef90050fe0c
thomas-montout <montoutthomas@gmail.com>
2026-03-16 13:03:53 +0100

    Compose app layout with new UI components

--- In the Cave/src/App.tsx
+++ In the Cave/src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import Stats from "./components/Stats";
+import Stats from "./Components/Stats";
+import Screen from "./Components/Screen";
+import Board from "./Components/Board";
+import GameText from "./Components/GameText";
+import Controls from "./Components/Controls";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <Stats health={100} gold={50} xp={25} />
+      <Screen>
+        <Stats health={100} gold={50} xp={25} />
+        <Board>
+          <GameText text="Welcome to the cave!" />
+          <Controls />
+        </Board>
+      </Screen>
     </>
   );
 }
