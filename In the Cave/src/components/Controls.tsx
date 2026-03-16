22193e08a490d88d0d2497ca45cb266c3d3d60af
thomas-montout <montoutthomas@gmail.com>
2026-03-16 13:03:50 +0100

    Add Controls button group component

---
+++ In the Cave/src/components/Controls.tsx
@@ -0,0 +1,17 @@
+function Controls() {
+  return (
+    <div className="flex flex-row gap-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-4 rounded">
+        Controls
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Controls
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Controls
+      </button>
+    </div>
+  );
+}
+
+export default Controls;
